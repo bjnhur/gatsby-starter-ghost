ca056bbc3629cf0abd666a5f296968000bf3c561
Bongjun Hur <bjnhur@gmail.com>
2022-08-12 17:53:18 +0900

    .

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,8 +1,7 @@
 const config = require(`../../.ghost.json`).production;
 module.exports = {
     siteUrl:
-        process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
+        process.env.NODE_ENV === `production` ? `https://bjnhur.github.io`
             : `http://localhost:8000`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
